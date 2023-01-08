563633919367fc01362d199b6529f17bddc39185
김승우 <rnqhqaltjs>
2023-01-08 22:01:40 +0900

    Initial commit

--- app/src/main/java/com/example/totalshopping2/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/totalshopping2/ui/view/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.totalshopping2.ui.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.totalshopping2.R
 import com.example.totalshopping2.data.repository.ItemSearchRepositoryImpl
 import com.example.totalshopping2.databinding.ActivityMainBinding
@@ -14,15 +17,17 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
     lateinit var itemSearchViewModel: ItemSearchViewModel
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        setupBottomNavigationView()
-        if (savedInstanceState == null) {
-            binding.bottomNavigationView.selectedItemId = R.id.fragment_search
-        }
+//        setupBottomNavigationView()
+//        if (savedInstanceState == null) {
+//            binding.bottomNavigationView.selectedItemId = R.id.fragment_search
+//        }
+        setupJetpackNavigation()
 
         val itemSearchRepository = ItemSearchRepositoryImpl()
         val factory = ItemSearchViewModelProviderFactory(itemSearchRepository, this)
@@ -30,29 +35,37 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun setupBottomNavigationView() {
-        binding.bottomNavigationView.setOnItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.fragment_search -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.frame_layout, SearchFragment())
-                        .commit()
-                    true
-                }
-                R.id.fragment_favorite -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.frame_layout, FavoriteFragment())
-                        .commit()
-                    true
-                }
-                R.id.fragment_settings -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.frame_layout, SettingsFragment())
-                        .commit()
-                    true
-                }
-                else -> false
-            }
-        }
+    private fun setupJetpackNavigation() {
+        val host = supportFragmentManager
+            .findFragmentById(R.id.itemsearch_nav_host_fragment) as NavHostFragment? ?: return
+        navController = host.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
+
     }
+
+//    private fun setupBottomNavigationView() {
+//        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+//            when (item.itemId) {
+//                R.id.fragment_search -> {
+//                    supportFragmentManager.beginTransaction()
+//                        .replace(R.id.frame_layout, SearchFragment())
+//                        .commit()
+//                    true
+//                }
+//                R.id.fragment_favorite -> {
+//                    supportFragmentManager.beginTransaction()
+//                        .replace(R.id.frame_layout, FavoriteFragment())
+//                        .commit()
+//                    true
+//                }
+//                R.id.fragment_settings -> {
+//                    supportFragmentManager.beginTransaction()
+//                        .replace(R.id.frame_layout, SettingsFragment())
+//                        .commit()
+//                    true
+//                }
+//                else -> false
+//            }
+//        }
+//    }
 }
